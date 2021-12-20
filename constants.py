7546a4c55d75ca208edc7af74ea52c9fff3ba20d
mdavid <mdavid@student.42.fr>
2021-12-20 16:28:28 +0100

    update

--- constants.py
+++ constants.py
@@ -1,51 +1,51 @@
 # =========================================================================== #
 # _____________________   |Definition des constantes|   _____________________ #
 # =========================================================================== #
-b_bonus = True
+b_bonus = False
 
 prec = 1e-6
 
 dct_tokens = {"(" : 1,
-			  ")" : 2,
-			  "+" : 3,
-			  "-" : 4,
-			  "/" : 5,
-			  "*" : 6,
-			  "=" : 7,
-			  "ˆ" : 8,
-			  "^" : 8,
-			  "UNKNOWN": 9,
-			  "VARIABLE": 10,
-			  "NUMBER": 11}
+              ")" : 2,
+              "+" : 3,
+              "-" : 4,
+              "/" : 5,
+              "*" : 6,
+              "=" : 7,
+              "ˆ" : 8,
+              "^" : 8,
+              "UNKNOWN": 9,
+              "VARIABLE": 10,
+              "NUMBER": 11}
 
 dct_operator_priority = {
-			  "+" : 1,
-			  "-" : 1,
-			  "/" : 2,
-			  "*" : 2,
-			  "ˆ" : 3,
-			  '^' : 3,
-			  "=" : 4}
+              "+" : 1,
+              "-" : 1,
+              "/" : 2,
+              "*" : 2,
+              "ˆ" : 3,
+              '^' : 3,
+              "=" : 4}
 
 # -- precedence of the different operators
 # the precedence corresponds to the operator priority.
 dct_precedence = {'=' : 0,
-				  '(' : 0,
-				  ')' : 0,
-				  '+' : 2,
-				  '-' : 2,
-				  '/' : 3,
-				  '*' : 3,
-				  'ˆ' : 4,
-				  '^' : 4}
+                  '(' : 0,
+                  ')' : 0,
+                  '+' : 2,
+                  '-' : 2,
+                  '/' : 3,
+                  '*' : 3,
+                  'ˆ' : 4,
+                  '^' : 4}
 
 # -- dictionnary with the operators only
 operators = [dct_tokens['+'],
-			 dct_tokens["-"],
-			 dct_tokens["/"],
-			 dct_tokens["*"],
-			 dct_tokens["ˆ"],
-			 dct_tokens['^']]
+             dct_tokens["-"],
+             dct_tokens["/"],
+             dct_tokens["*"],
+             dct_tokens["ˆ"],
+             dct_tokens['^']]
 
 
 # -- operator(s) with a right/left side associativity
